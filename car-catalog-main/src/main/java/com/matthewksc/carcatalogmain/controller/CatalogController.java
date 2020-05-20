2d394875e183735b2345a06341a4ee82fcf15953
MatthewKsc <ksciukmateusz@gmail.com>
2020-05-20 19:44:46 +0200

    imports clean

--- car-catalog-main/src/main/java/com/matthewksc/carcatalogmain/controller/CatalogController.java
+++ car-catalog-main/src/main/java/com/matthewksc/carcatalogmain/controller/CatalogController.java
@@ -2,16 +2,12 @@ package com.matthewksc.carcatalogmain.controller;
 
 import com.matthewksc.carcatalogmain.dao.enityty.CatalogItem;
 import com.matthewksc.carcatalogmain.service.CatalogItemService;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.util.Arrays;
-import java.util.List;
 
 @RestController
 @RequestMapping("/catalog")
